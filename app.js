be3595d05f8e307d4f888b00c1ef86d388682610
P Gana <gana.psuresh@gmail.com>
2020-07-19 19:10:39 +0530

    Setting process.env.port config, and package file changes

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 var express = require('express');
 var app = express();
 
- 
-
- app.get('/get', function(req, res) {
-    res.status(400).send({status:'success', message:'NodeJS apis are working as expected -T1'});
- });
 
 
-app.listen(3000, ()=>{
-    console.log('Server is listening at 3000 port');
-})
+app.get('/get', function (req, res) {
+    res.status(400).send({ status: 'success', message: 'NodeJS apis are working as expected -T1' });
+});
+
+
+
+/// THis line is so important (process.env.PORT) becase, cloud picks random port numbers.
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server is listening at port : ' + port);
+});
